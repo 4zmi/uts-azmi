cea2d06091ffe89066967730a44efdb54901f81f
4zmi <70999284+4zmi@users.noreply.github.com>
2020-09-09 14:03:45 +0700

    Add files via upload

---
+++ substring.java
@@ -0,0 +1,13 @@
+package utsazmi;
+
+public class substring {
+    public static void main(String[] args) {
+        String alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ#-";
+
+        System.out.println(alfabet);
+        System.out.println("" + alfabet.substring(0, 1));
+        System.out.println("" + alfabet.substring(25, 26));
+        System.out.println("" + alfabet.substring(12, 13));
+        System.out.println("" + alfabet.substring(8, 9));
+    }
+}
